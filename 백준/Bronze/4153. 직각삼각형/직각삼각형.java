8f11ea828332aafb367ac0cd88eec5c04f2be0e1
leeyoosong <70739179+aramarabara@users.noreply.github.com>
2026-01-07 18:37:18 +0900

    [Bronze III] Title: 직각삼각형, Time: 100 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        while (true) {
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if (a == 0 && b == 0 && c == 0
+               ) {
+                break;
+            }
+            if (a * a + b * b == c * c
+                || a * a + c * c == b * b
+                || b * b + c * c == a * a
+               ) {
+                bw.write("right\n");
+            } else {
+                bw.write("wrong\n");
+            }
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
